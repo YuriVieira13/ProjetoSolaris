50b786ccb2f6adac90deda70e24d791f52038112
giovanna-tracinkas <giovanna.tmenesio@sptech.school>
2026-03-29 17:44:46 -0300

    Adicionando arquivo de validacao de credenciais (vazio)

---
+++ Protótipos/Protótipo_Solaris_CSS/public/validacaoCredenciais.js
@@ -0,0 +1,42 @@
+  function validarEmail(){
+        let email = email_input.value;
+        let emailMinusculo = email.toLowerCase();
+
+        if(email.length >){
+
+        }
+
+        div_verificarEmail.innerHTML = resposta;
+    }
+
+    function cadastrar() {
+        // Váriaveis de credenciais inseridas pelo usuário.
+        let nome = nome_input.value;
+        let senha = senha_input.value;
+        let senhaMinusculo = senha.toLowerCase();
+
+        //Váriaveis de verificação de inclusão de caracteres necessários.
+        let temNumero = false;
+        let temLetra = false;
+        let temCaracterEspecial = false;
+
+        //Lista de caractere necessários para cadastro no sistema.
+        let caracteresEspeciais = ['_', '-', '@', '!', '#', '$', '&', '*', '?', '%', '+', '.', ':'];
+
+        let numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+        let letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        // loop para verificação de caracteres necessários para senha.
+
+        if(){
+
+        }
+        for (let i = 0; i < senha.length; i++) { // percorre todos os caracteres da senha
+            if (senhaMinusculo.includes(caracteresEspeciais)) {
+            }
+
+        }
+
+    }
