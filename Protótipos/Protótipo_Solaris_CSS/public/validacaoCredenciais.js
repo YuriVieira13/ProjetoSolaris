deb001a85b82e9cbdb639d15460a203afbf63327
giovanna-tracinkas <giovanna.tmenesio@sptech.school>
2026-04-03 22:04:10 -0300

    adicionando validação de email

--- Protótipos/Protótipo_Solaris_CSS/public/validacaoCredenciais.js
+++ Protótipos/Protótipo_Solaris_CSS/public/validacaoCredenciais.js
@@ -1,42 +1,39 @@
-  function validarEmail(){
-        let email = email_input.value;
-        let emailMinusculo = email.toLowerCase();
+function validarEmail() {
+    let emailInserido = email_input.value;
+    let resposta = `E-mail válido`;
 
-        if(email.length >){
+    if (emailInserido != '') {
+        if (emailInserido.length <= 256) { 
+            if (emailInserido.indexOf('@') == -1 || emailInserido.indexOf('.') == -1) {
+                if (emailInserido.indexOf('.') == -1) {
+                    resposta = `E-mail inválido. Precisa conter '.'`
+                }
+                if (emailInserido.indexOf('@') == -1) {
+                    resposta = `E-mail inválido. Precisa conter '@'`
+                }
+            } else {
+                let qtdArroba = 0;
 
-        }
-
-        div_verificarEmail.innerHTML = resposta;
-    }
-
-    function cadastrar() {
-        // Váriaveis de credenciais inseridas pelo usuário.
-        let nome = nome_input.value;
-        let senha = senha_input.value;
-        let senhaMinusculo = senha.toLowerCase();
-
-        //Váriaveis de verificação de inclusão de caracteres necessários.
-        let temNumero = false;
-        let temLetra = false;
-        let temCaracterEspecial = false;
-
-        //Lista de caractere necessários para cadastro no sistema.
-        let caracteresEspeciais = ['_', '-', '@', '!', '#', '$', '&', '*', '?', '%', '+', '.', ':'];
-
-        let numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-        let letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-        // loop para verificação de caracteres necessários para senha.
-
-        if(){
-
-        }
-        for (let i = 0; i < senha.length; i++) { // percorre todos os caracteres da senha
-            if (senhaMinusculo.includes(caracteresEspeciais)) {
+                for (let i = 0; i < emailInserido.length; i++) {
+                    if (emailInserido[i] == '@') {
+                        qtdArroba++;
+                    }
+                    if (emailInserido[i] == '.') {
+                        if (emailInserido[i - 1] == '.') {
+                            resposta = ` E-mail inválido. Não pode ter dois pontos consecutivos (..)`
+                        }
+                    }
+                }
+                if (qtdArroba > 1) {
+                    resposta = `E-mail inválido. Não pode ter mais de um '@'`
+                }
             }
-
+        } else {
+            resposta = `E-mail muito longo. Limite de caracteres: 256`
         }
-
     }
+    else {
+        resposta = ` Preencha o campo e-mail para continuar`
+    }
+    div_verificarEmail.innerHTML = resposta;
+}
