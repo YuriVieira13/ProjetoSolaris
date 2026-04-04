3767e36a90c40386db11f1976b250bf8fb349cf3
giovanna-tracinkas <giovanna.tmenesio@sptech.school>
2026-04-03 23:12:38 -0300

    adicionando validação de carácter inicial

--- Protótipos/Protótipo_Solaris_CSS/public/validacaoCredenciais.js
+++ Protótipos/Protótipo_Solaris_CSS/public/validacaoCredenciais.js
@@ -1,31 +1,35 @@
 function validarEmail() {
     let emailInserido = email_input.value;
+    let emailMinusculo = emailInserido.toLowerCase();
     let resposta = `E-mail válido`;
 
     if (emailInserido != '') {
-        if (emailInserido.length <= 256) { 
-            if (emailInserido.indexOf('@') == -1 || emailInserido.indexOf('.') == -1) {
-                if (emailInserido.indexOf('.') == -1) {
-                    resposta = `E-mail inválido. Precisa conter '.'`
-                }
-                if (emailInserido.indexOf('@') == -1) {
-                    resposta = `E-mail inválido. Precisa conter '@'`
-                }
+        if (emailInserido.length <= 256) {
+            if (emailMinusculo[0] < 'a' || emailMinusculo[0] > 'z') {
+                resposta = `E-mail precisa começar com letra`
             } else {
-                let qtdArroba = 0;
-
-                for (let i = 0; i < emailInserido.length; i++) {
-                    if (emailInserido[i] == '@') {
-                        qtdArroba++;
+                if (emailInserido.indexOf('@') == -1 || emailInserido.indexOf('.') == -1) {
+                    if (emailInserido.indexOf('.') == -1) {
+                        resposta = `E-mail inválido. Precisa conter '.'`
                     }
-                    if (emailInserido[i] == '.') {
-                        if (emailInserido[i - 1] == '.') {
-                            resposta = ` E-mail inválido. Não pode ter dois pontos consecutivos (..)`
+                    if (emailInserido.indexOf('@') == -1) {
+                        resposta = `E-mail inválido. Precisa conter '@'`
+                    }
+                } else {
+                    let qtdArroba = 0;
+                    for (let i = 0; i < emailInserido.length; i++) {
+                        if (emailInserido[i] == '@') {
+                            qtdArroba++;
+                        }
+                        if (emailInserido[i] == '.') {
+                            if (emailInserido[i - 1] == '.') {
+                                resposta = ` E-mail inválido. Não pode ter dois pontos consecutivos (..)`
+                            }
                         }
                     }
-                }
-                if (qtdArroba > 1) {
-                    resposta = `E-mail inválido. Não pode ter mais de um '@'`
+                    if (qtdArroba > 1) {
+                        resposta = `E-mail inválido. Não pode ter mais de um '@'`
+                    }
                 }
             }
         } else {
@@ -33,7 +37,7 @@ function validarEmail() {
         }
     }
     else {
-        resposta = ` Preencha o campo e-mail para continuar`
+        resposta = `Preencha o campo e-mail para continuar`
     }
     div_verificarEmail.innerHTML = resposta;
 }
