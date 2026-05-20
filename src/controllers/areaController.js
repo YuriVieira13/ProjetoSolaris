4864c5aba4a80bf3cee1a150f3e0cf2f8277bc0e
YuriVieira13 <yuri.avieira@sptech.school>
2026-05-19 21:00:36 -0300

    tela area plantil 90% finalizada

--- src/controllers/areaController.js
+++ src/controllers/areaController.js
@@ -2,7 +2,7 @@ var areaModel = require("../models/areaModel");
 
 function listar(req, res) {
       var areaPlantil = req.params.areaPlantil;
-      var hectare = req.params.areaPlantil;
+      var hectare = req.params.hectare;
     areaModel.listar(areaPlantil, hectare).then(function(resultado){
         // precisamos informar que o resultado voltará para o front-end como uma resposta em json
         res.status(200).json(resultado);
