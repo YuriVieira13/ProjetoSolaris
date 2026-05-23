b68b363c0b416b34f75b532499cb6dac7c6b1b64
giovanna-tracinkas <giovanna.tmenesio@sptech.school>
2026-05-23 11:35:28 -0300

    feat: adicionando função de busca de alertas

--- src/models/areaModel.js
+++ src/models/areaModel.js
@@ -12,8 +12,27 @@ function listar(areaPlantil, hectare) {
     return database.executar(instrucao);
 }
 
+function buscarAlertas(idUsuario, idFazenda) {
+    var instrucao = `
+    SELECT h.idHectare, l.ValorLeitura, ap.nome,
+CASE 
+	WHEN l.ValorLeitura < 800 THEN 'BAIXA LUMINOSIDADE'
+    WHEN l.ValorLeitura > 1500 THEN 'ALTA LUMINOSIDADE'
+    ELSE 'LUMINOSIDADE IDEAL'
+    END AS 'Nível alerta'
+FROM funcionario func JOIN empresa ON func.fkEmpresa = empresa.idEmpresa
+JOIN fazenda faz ON faz.fkEmpresa = empresa.idEmpresa
+JOIN hectare h JOIN areaPlantio ap ON ap.idAreaPlantio = h.fkAreaPlantio
+JOIN sensor s ON s.fkHectare = h.idHectare 
+JOIN leitura l ON l.fkSensor = s.idSensor;
+WHERE func.idUsuario = ${idUsuario} AND faz.idFazenda = ${idFazenda};`;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 
 
 module.exports = {
-    listar
+    listar,
+    buscarAlertas
 };
