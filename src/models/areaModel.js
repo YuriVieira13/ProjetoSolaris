3c853c57e8b8af077152a433959ceef15fb7601a
giovanna-tracinkas <giovanna.tmenesio@sptech.school>
2026-05-23 17:48:15 -0300

    fix: corrigindo select de alertas da model

--- src/models/areaModel.js
+++ src/models/areaModel.js
@@ -19,13 +19,13 @@ CASE
 	WHEN l.ValorLeitura < 800 THEN 'BAIXA LUMINOSIDADE'
     WHEN l.ValorLeitura > 1500 THEN 'ALTA LUMINOSIDADE'
     ELSE 'LUMINOSIDADE IDEAL'
-    END AS 'Nível alerta'
+    END AS 'Alerta'
 FROM funcionario func JOIN empresa ON func.fkEmpresa = empresa.idEmpresa
 JOIN fazenda faz ON faz.fkEmpresa = empresa.idEmpresa
 JOIN hectare h JOIN areaPlantio ap ON ap.idAreaPlantio = h.fkAreaPlantio
 JOIN sensor s ON s.fkHectare = h.idHectare 
-JOIN leitura l ON l.fkSensor = s.idSensor;
-WHERE func.idUsuario = ${idUsuario} AND faz.idFazenda = ${idFazenda};`;
+JOIN leitura l ON l.fkSensor = s.idSensor
+WHERE func.idUsuario = ${idUsuario} AND faz.fkEmpresa = ${idFazenda};`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
