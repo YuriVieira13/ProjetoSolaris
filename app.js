adb930f7b6b400fd043d4ba7e2b136b17e5a3c8e
Guilherme-CunhaRdl <guilhermecunha928@gmail.com>
2026-05-23 17:30:41 -0300

    Integrando o BobAI

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
+const { GoogleGenAI } = require("@google/genai");
 var cors = require("cors");
 var path = require("path");
 var PORTA_APP = process.env.APP_PORT;
@@ -53,3 +54,42 @@ app.listen(PORTA_APP, function () {
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
+
+
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
+// rota para receber perguntas e gerar respostas
+app.post("/perguntar", async (req, res) => {
+  const pergunta = req.body.pergunta;
+
+  try {
+      const resultado = await gerarResposta(pergunta);
+      res.json({ resultado });
+  } catch (error) {
+      res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+
+});
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+
+  try {
+      // gerando conteúdo com base na pergunta
+      const modeloIA = chatIA.models.generateContent({
+          model: "gemini-2.5-flash",
+          contents: `Em um paragráfo responda: ${mensagem}`
+
+      });
+      const resposta = (await modeloIA).text;
+      const tokens = (await modeloIA).usageMetadata;
+
+      console.log(resposta);
+      console.log("Uso de Tokens:", tokens);
+
+      return resposta;
+  } catch (error) {
+      console.error(error);
+      throw error;
+  }
+}
