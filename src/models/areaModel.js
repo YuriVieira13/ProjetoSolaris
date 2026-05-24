f29c637790aed39583cdd3389aa946d2d98f17e2
giovanna-tracinkas <giovanna.tmenesio@sptech.school>
2026-05-23 22:29:53 -0300

    fix: corrigindo select de alertas

--- src/models/areaModel.js
+++ src/models/areaModel.js
@@ -14,17 +14,20 @@ function listar(areaPlantil, hectare) {
 
 function buscarAlertas(idUsuario, idFazenda) {
     var instrucao = `
-    SELECT h.idHectare, l.ValorLeitura, ap.nome,
+    SELECT h.idHectare, l.ValorLeitura, ap.nome, faz.nome as nome_fazenda, alerta.descricao,
 CASE 
 	WHEN l.ValorLeitura < 800 THEN 'BAIXA LUMINOSIDADE'
     WHEN l.ValorLeitura > 1500 THEN 'ALTA LUMINOSIDADE'
     ELSE 'LUMINOSIDADE IDEAL'
     END AS 'Alerta'
-FROM funcionario func JOIN empresa ON func.fkEmpresa = empresa.idEmpresa
-JOIN fazenda faz ON faz.fkEmpresa = empresa.idEmpresa
-JOIN hectare h JOIN areaPlantio ap ON ap.idAreaPlantio = h.fkAreaPlantio
-JOIN sensor s ON s.fkHectare = h.idHectare 
+FROM funcionario func
+JOIN empresa e ON func.fkEmpresa = e.idEmpresa
+JOIN fazenda faz ON faz.fkEmpresa = e.idEmpresa
+JOIN areaplantio ap ON ap.fkFazenda = faz.idFazenda
+JOIN hectare h ON h.fkAreaPlantio = ap.idAreaPlantio
+JOIN sensor s ON s.fkHectare = h.idHectare
 JOIN leitura l ON l.fkSensor = s.idSensor
+JOIN alerta ON alerta.fkHectare = h.idHectare
 WHERE func.idUsuario = ${idUsuario} AND faz.fkEmpresa = ${idFazenda};`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
