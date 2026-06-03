a975ad0cb81ac62a06224f0835e402cef67cb07e
vraimundosptech <vitor.raimundo@sptech.school>
2026-06-03 15:11:19 -0300

    deixando tela dashboard.html dinâmica

--- src/controllers/areaController.js
+++ src/controllers/areaController.js
@@ -1,9 +1,18 @@
 var areaModel = require("../models/areaModel");
 
-function listar(req, res) {
+function listarArea(req, res) {
+      var idUsuario = req.params.idUsuario
+    areaModel.listarArea(idUsuario).then(function(resultado){
+        res.status(200).json(resultado);
+    }).catch(function(erro){
+        res.status(500).json(erro.sqlMessage);
+    })
+}
+
+function listarLuz(req, res) {
       var areaPlantil = req.params.areaPlantil;
       var hectare = req.params.hectare;
-    areaModel.listar(areaPlantil, hectare).then(function(resultado){
+    areaModel.listarLuz(areaPlantil, hectare).then(function(resultado){
         // precisamos informar que o resultado voltará para o front-end como uma resposta em json
         res.status(200).json(resultado);
     }).catch(function(erro){
@@ -22,6 +31,7 @@ function buscarAlertas(req, res) {
 }
 
 module.exports = {
-    listar,
+    listarArea,
+    listarLuz,
     buscarAlertas
 }
--- src/models/areaModel.js
+++ src/models/areaModel.js
@@ -1,7 +1,19 @@
 var database = require("../database/config")
 
+function listarArea(idUsuario) {
+    var instrucao = `
+    SELECT a.idAreaPlantio FROM fazenda fa
+    JOIN areaPlantio a
+        ON a.fkFazenda = fa.idFazenda
+    JOIN funcionario fu
+        ON fu.fkFazenda = fa.idFazenda
+    WHERE fu.idUsuario = ${idUsuario};
+    `
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
 
-function listar(areaPlantil, hectare) {
+function listarLuz(areaPlantil, hectare) {
     var instrucao = `
      SELECT * FROM vw_leitura_min_max_semanal
     WHERE idAreaPlantio = ${areaPlantil}
@@ -15,20 +27,20 @@ function listar(areaPlantil, hectare) {
 function buscarAlertas(idUsuario, idFazenda) {
     var instrucao = `
     SELECT h.idHectare, l.ValorLeitura, ap.nome, faz.nome as nome_fazenda, alerta.descricao,
-CASE 
-	WHEN l.ValorLeitura < 800 THEN 'BAIXA LUMINOSIDADE'
-    WHEN l.ValorLeitura > 1500 THEN 'ALTA LUMINOSIDADE'
-    ELSE 'LUMINOSIDADE IDEAL'
-    END AS 'Alerta'
-FROM funcionario func
-JOIN empresa e ON func.fkEmpresa = e.idEmpresa
-JOIN fazenda faz ON faz.fkEmpresa = e.idEmpresa
-JOIN areaplantio ap ON ap.fkFazenda = faz.idFazenda
-JOIN hectare h ON h.fkAreaPlantio = ap.idAreaPlantio
-JOIN sensor s ON s.fkHectare = h.idHectare
-JOIN leitura l ON l.fkSensor = s.idSensor
-JOIN alerta ON alerta.fkHectare = h.idHectare
-WHERE func.idUsuario = ${idUsuario} AND faz.fkEmpresa = ${idFazenda};`;
+        CASE 
+            WHEN l.ValorLeitura < 800 THEN 'BAIXA LUMINOSIDADE'
+            WHEN l.ValorLeitura > 1500 THEN 'ALTA LUMINOSIDADE'
+            ELSE 'LUMINOSIDADE IDEAL'
+            END AS 'Alerta'
+        FROM funcionario func
+        JOIN empresa e ON func.fkEmpresa = e.idEmpresa
+        JOIN fazenda faz ON faz.fkEmpresa = e.idEmpresa
+        JOIN areaplantio ap ON ap.fkFazenda = faz.idFazenda
+        JOIN hectare h ON h.fkAreaPlantio = ap.idAreaPlantio
+        JOIN sensor s ON s.fkHectare = h.idHectare
+        JOIN leitura l ON l.fkSensor = s.idSensor
+        JOIN alerta ON alerta.fkHectare = h.idHectare
+        WHERE func.idUsuario = ${idUsuario} AND faz.fkEmpresa = ${idFazenda};`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
@@ -36,6 +48,7 @@ WHERE func.idUsuario = ${idUsuario} AND faz.fkEmpresa = ${idFazenda};`;
 
 
 module.exports = {
-    listar,
+    listarArea,
+    listarLuz,
     buscarAlertas
 };
