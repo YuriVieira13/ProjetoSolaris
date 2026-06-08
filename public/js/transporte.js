5182fad68ad59c0e868e9cda5ccb2ec9fda8cc30
Vitor Machado <vitor.raimundo@sptech.school>
2026-06-07 22:48:18 -0300

    arrumando posicao da area de plantio

--- public/js/transporte.js
+++ public/js/transporte.js
@@ -1,5 +1,5 @@
-function direcionar(numero) {
-
+function direcionar(numero, posicao) {
+sessionStorage.POSICAO = posicao
 sessionStorage.ID_AREA_PLANTIL = numero;
 window.location.href = "./Area1/index.html"
 }
